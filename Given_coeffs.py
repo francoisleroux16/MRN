014bf95075fcbb5252ef16869de68a809db61b45
fleroux@mit.edu <fleroux@mit.edu>
2020-06-26 13:25:42 +0200

    Update

--- Given_coeffs.py
+++ Given_coeffs.py
@@ -29,7 +29,7 @@ for x in range(len(probe_data)):
 
 counter = 0
 for j in range(len(pitch_vals)):
-    plt.figure('C_py_new2')
+    # plt.figure('C_pp')
     pitch = pitch_vals[j]
     plot = np.zeros((len(yaw_vals),5))
     for x in range(len(yaw_vals)):
@@ -43,49 +43,57 @@ for j in range(len(pitch_vals)):
         plot[x,3] = Cpts
         plot[x,4] = pitch
         counter += 1
-    plt.scatter(plot[:,0],plot[:,1],label=r'Pitch = '+str(pitch)+r'$\degree$') #Cpy
-    plt.plot(plot[:,0],plot[:,1])
+    # plt.scatter(plot[:,0],plot[:,1],label=r'Pitch = '+str(pitch)+r'$\degree$') #Cpy
+    # plt.plot(plot[:,0],plot[:,1])
+    # plt.scatter(plot[:,0],plot[:,2],label=r'Pitch = '+str(pitch)+r'$\degree$') #Cpp w.r.t. yaw
+    # plt.plot(plot[:,0],plot[:,2])
     # plt.scatter(plot[:,0],plot[:,3],label=r'Pitch = '+str(pitch)+r'$\degree$') #Cpts
     # plt.plot(plot[:,0],plot[:,3])
-    # plt.scatter(plot[:,1],plot[:,2])
+    # plt.scatter(plot[:,1],plot[:,2]) #Cpy vs Cpp
     # plt.plot(plot[:,1],plot[:,2], label=r'Pitch = '+str(pitch)+r'$\degree$')
 
 ## Only for carpet plot
 
 # first_legend = plt.legend(loc='upper left', bbox_to_anchor=(1.001,1), borderaxespad =0)   # Right Hand Side
 
-# index2 = np.lexsort((probe_data[:,0],probe_data[:,1])) #sorted yaw then pitch
-# counter2 = 0
-# yaw_label = []
-# for k in range(len(yaw_vals)):
-#     plt.figure('C_py_new')
-#     yaw = yaw_vals[k]
-#     plot2 =np.zeros((len(pitch_vals),5))
-#     for z in range(len(pitch_vals)):
-#         Cpy = probe_data[index2[counter2],2]
-#         Cpp = probe_data[index2[counter2],3]
-#         Cpts = probe_data[index2[counter2],4]
-#         counter2 += 1
-#         plot2[z,0] = yaw
-#         plot2[z,1] = Cpy
-#         plot2[z,2] = Cpp
-#         plot2[z,3] = Cpts
-#         plot2[z,4] = pitch_vals[z]
+index2 = np.lexsort((probe_data[:,0],probe_data[:,1])) #sorted yaw then pitch
+counter2 = 0
+# yaw_label = [] #for carpet
+for k in range(len(yaw_vals)):
+    plt.figure('C_pts_new')
+    yaw = yaw_vals[k]
+    plot2 =np.zeros((len(pitch_vals),5))
+    for z in range(len(pitch_vals)):
+        Cpy = probe_data[index2[counter2],2]
+        Cpp = probe_data[index2[counter2],3]
+        Cpts = probe_data[index2[counter2],4]
+        counter2 += 1
+        plot2[z,0] = yaw
+        plot2[z,1] = Cpy
+        plot2[z,2] = Cpp
+        plot2[z,3] = Cpts
+        plot2[z,4] = pitch_vals[z]
 #     yaw_label.append(r'Yaw = '+str(yaw)+r'$\degree$')
 #     # plt.scatter(plot2[:,1],plot2[:,2])
 #     plt.plot(plot2[:,1],plot2[:,2]) #, label=r'Yaw = '+str(yaw)+r'$\degree$')
 #     # plt.scatter(plot2[:,4],plot2[:,1],label=r'Pitch = '+str(pitch)+r'$\degree$')
 #     # plt.plot(plot2[:,4],plot2[:,1])
+    # plt.scatter(plot2[:,4],plot2[:,2], label=r'Yaw = '+str(yaw)+r'$\degree$') #Cpp w.r.t. pitch
+    # plt.plot(plot2[:,4],plot2[:,2]) 
+    plt.scatter(plot2[:,4],plot2[:,3], label=r'Yaw = '+str(yaw)+r'$\degree$') #Cpts w.r.t. pitch
+    plt.plot(plot2[:,4],plot2[:,3]) 
         
 # first_legend = plt.legend(loc='upper left', bbox_to_anchor=(1.001,1), borderaxespad =0)   # Right Hand Side
 # ax = plt.gca().add_artist(first_legend)
 # plt.legend(labels = yaw_label, loc='upper left', bbox_to_anchor=(1.001,0.35), borderaxespad=0)
 # ax = plt.gca().add_artist(first_legend)
+# plt.legend(loc='upper left', bbox_to_anchor=(1.001,1), borderaxespad=0)
 plt.legend()
-plt.grid(b=True, which='major', color='#666666', linestyle='-')
-plt.minorticks_on()
-plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
-plt.xlabel(r"$C_{py}$",fontsize=16)
-plt.ylabel(r"$C_{pp}$",fontsize=16)
-plt.title(r'$C_{py}$ vs $C_{pp}$ ')
-plt.show()    
+# plt.grid(b=True, which='major', color='#666666', linestyle='-') #carpet
+# plt.minorticks_on() #carpet
+# plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2) #carpet
+# plt.xlabel(r"$C_{py}$",fontsize=16) #carpet
+plt.xlabel(r"Pitch Angle [$\degree$]",fontsize=16)
+plt.ylabel(r"$C_{pts}$",fontsize=16)
+plt.title(r'$C_{pts}$ vs Pitch ($\alpha$)',fontsize=16)
+plt.show()
